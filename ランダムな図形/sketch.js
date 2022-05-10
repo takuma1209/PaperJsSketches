f02caf3fb504932e6318661de7e6469f25b4bfb9
Narahara Takuma <takuma2021pk@nzu.jp>
2022-05-10 16:10:15 +0900

    追加

--- ランダムな図形/sketch.js
+++ ランダムな図形/sketch.js
@@ -1,12 +1,32 @@
 // ここにスケッチ名を指定することができる。
-let sketchName = 'My Sketch';
+let sketchName = 'ランダム雲';
 
-// ロードが完了した時に実行する関数。ここで初期化の処理を書く。
-// P5.jsやProcessingのsetupと同じ役割。
+function pickRandom(array){
+    let i = Math.floor(Math.random() * array/length);
+    return array[i];
+}
+
+let randomRangeX = 30;
+let ramdomRangeY = 20;
+
+function drawCloud(x,y){
+     for (let i = 0; i < 10; i++){
+         let rand_x = Math.random() * randomRangeX - randomRangeX * 0.5;
+         let rand_y = Math.random() * randomRAngeY - randomRangeY * 0.5;
+         let sizeX = Math.random() * 20 + 20;
+         let sizeY = Math.random() * 10 + 10;
+         Path.Oval([x + rand_x, y + rand_y],[sizeX, sizeY]);
+     }
+}
+
+
+
 window.addEventListener('load', function(){
     // キャンバスの大きさを指定する。
     view.viewSize = new Size(500, 500);
 
+    let bg = Path.Rectangle([0,0],view.viewSize);
+    
     let x = view.viewSize.width * 0.25;
     let y = view.viewSize.height * 0.3;
     let rectSize = [200, 300];
