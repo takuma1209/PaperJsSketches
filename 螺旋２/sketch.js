11a236a984bcc3f3bc8a6b50d91881e421e1a0c7
Narahara Takuma <takuma2021pk@nzu.jp>
2022-05-17 16:08:20 +0900

    名称変更

--- 螺旋２/sketch.js
+++ 螺旋２/sketch.js
@@ -1,32 +1,20 @@
-// ここにスケッチ名を指定することができる。
-let sketchName = 'My Sketch';
 
-// ロードが完了した時に実行する関数。ここで初期化の処理を書く。
-// P5.jsやProcessingのsetupと同じ役割。
-window.addEventListener('load', function(){
-    // キャンバスの大きさを指定する。
-    view.viewSize = new Size(500, 500);
-    project.currentStyle = {
-        strokeColor: '#6C9AE0', // 線の色
-        fillColor: '#A5F2F2', // 塗りの色
-        strokeWidth: 0.5 // 線の幅
-    };
+let skethName = "狂乱"
 
-    let x = view.viewSize.width * 0.5;
-    let y = view.viewSize.height * 0.5;
 
-    let count = 350;
-    let length = 0;
-    let angle = 3; 
-    let theta = 100;
+function drawSpiral(x,y){
+    let count = 1000;
+    let angle = Math.PI * 0.36;
+    let length = 10 + Math.random() * 10;
+    let dl = 0.1 + Math.random() * 0.9;
+    let randomRange = 0;
 
-    for (let i = 0; i < count; i++) {
-        let p = Path.Circle([x + length, y], 50);
-        p.rotate(theta, [x, y]);
-        theta += angle;
-        length += 1;
-    }
-   
-    // 画面を描く。
-    view.draw();
-});
+    let points =[];
+    let theata = Math.PI * 0.25;
+
+    for (let i = 0;i < count; i++){
+        points.push([x,y]);
+　
+
+     }
+}
